663cbab1b1b25e750c4eed8b953d37fc99d373b9
Nagabhushanam <nagabhushanamn@gmail.com>
2020-11-25 01:25:18 +0530

    day2

--- play-with-javascript/index.js
+++ play-with-javascript/index.js
@@ -1,1 +1,41 @@
 
+//--------------------------------
+// obj literal enhancements
+//--------------------------------
+
+//--------------------------------
+// ES5
+//--------------------------------
+let name = "Nag"
+let age = 36
+
+let person = {
+    name: name,
+    age: age,
+    sayName: function () {
+        //..
+    },
+    3: 'three',
+    'home-address': 'chennai'
+}
+
+//--------------------------------
+// ES6
+//--------------------------------
+
+let addressType = "office"  // office | vacation
+
+let person2 = {
+    name,
+    age,
+    sayName() {
+        //..
+    },
+    [1 + 2]: 'three',
+    [addressType + "-address"]: 'chennai-india',
+    'say Hi'() {
+        console.log("hi")
+    }
+}
+
+person2['say Hi']()
