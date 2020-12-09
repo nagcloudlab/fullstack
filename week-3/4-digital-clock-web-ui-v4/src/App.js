16af794cdc255264f33fe5d5ede860413457afad
Nagabhushanam <nagabhushanamn@gmail.com>
2020-12-09 23:18:19 +0530

    using create-react-app tool

---
+++ week-3/4-digital-clock-web-ui-v4/src/App.js
@@ -0,0 +1,49 @@
+
+
+function Clock(props) {
+  let { timeZone } = props
+  return (
+    <div className="card">
+      <div className="card-header">{timeZone}</div>
+      <div className="card-body">
+        <span className="badge badge-dark">{new Date().toLocaleTimeString('en', { timeZone })}</span>
+      </div>
+    </div>
+  )
+}
+
+
+function AnalogClock(){
+  return
+}
+
+function Board() {
+  return (
+    <div className="row">
+      <div className="col-4">
+        <Clock timeZone="Asia/Kolkata" />
+      </div>
+      <div className="col-4">
+        <Clock timeZone="Asia/Dubai" />
+      </div>
+      <div className="col-4">
+        <Clock timeZone="America/New_york" />
+      </div>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div className="container">
+
+      <hr />
+      <h1>world clocks : <small>powered by react.js</small></h1>
+      <hr />
+      <Board />
+
+    </div>
+  );
+}
+
+export default App;
