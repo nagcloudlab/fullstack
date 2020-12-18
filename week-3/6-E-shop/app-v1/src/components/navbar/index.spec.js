9a75427430d2dae85d106a1bbdc0c4a4cfc4292c
Nagabhushanam <nagabhushanamn@gmail.com>
2020-12-18 20:12:29 +0530

    fullscreen api

---
+++ week-3/6-E-shop/app-v1/src/components/navbar/index.spec.js
@@ -0,0 +1,23 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Navbar from './index'
+import { render, cleanup } from '@testing-library/react'
+
+import renderer from 'react-test-renderer'
+
+afterEach(cleanup);
+
+it('render without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<Navbar />, div)
+});
+it('renders navbar correctly', () => {
+    const { getByText } = render(<Navbar title="e-shop" />)
+    expect(getByText('e-shop')).toHaveTextContent('e-shop')
+})
+it('matches snapshot', () => {
+    const tree = renderer.create(<Navbar title="e-shop" />).toJSON()
+    expect(tree).toMatchSnapshot();
+})
