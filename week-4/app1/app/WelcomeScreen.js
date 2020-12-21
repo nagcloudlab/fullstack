018e44be79eef96e525d31ace4313c9d686a7252
Nagabhushanam <nagabhushanamn@gmail.com>
2020-12-22 00:36:59 +0530

    app1

---
+++ week-4/app1/app/WelcomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native'
+
+function WelcomeScreen(props) {
+    return (
+        <View>
+            <Text>Welcome screen</Text>
+        </View>
+    );
+}
+
+export default WelcomeScreen;
