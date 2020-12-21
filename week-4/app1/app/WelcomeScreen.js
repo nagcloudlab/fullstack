bf7d361ce535b5a1a1d5981981cfd83a718a8a7b
Nagabhushanam <nagabhushanamn@gmail.com>
2020-12-22 01:28:26 +0530

    welcome screen

--- week-4/app1/app/WelcomeScreen.js
+++ week-4/app1/app/WelcomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, ImageBackground, StyleSheet } from 'react-native'
+import { View, Text, Image, ImageBackground, StyleSheet } from 'react-native'
 
 function WelcomeScreen(props) {
     return (
@@ -7,7 +7,13 @@ function WelcomeScreen(props) {
             style={styles.background}
             source={require("./assets/background.jpg")}
         >
+            <View style={styles.logoContainer}>
+                <Image style={styles.logo} source={require("./assets/logo-red.png")} />
+                <Text>Sell What You Don't Need</Text>
+            </View>
 
+            <View style={styles.loginButton}></View>
+            <View style={styles.registerButton}></View>
         </ImageBackground>
     );
 }
@@ -15,9 +21,29 @@ function WelcomeScreen(props) {
 const styles = StyleSheet.create({
     background: {
         flex: 1,
-        justifyContent: 'center',
+        justifyContent: 'flex-end',
         alignItems: 'center'
-    }
+    },
+    loginButton: {
+        width: '100%',
+        height: 70,
+        backgroundColor: '#fc5c65'
+    },
+    logo: {
+        width: 100,
+        height: 100,
+
+    },
+    logoContainer: {
+        position: 'absolute',
+        top: 70,
+        alignItems: 'center'
+    },
+    registerButton: {
+        width: "100%",
+        height: 70,
+        backgroundColor: "#4ecdc4",
+    },
 });
 
 export default WelcomeScreen;
