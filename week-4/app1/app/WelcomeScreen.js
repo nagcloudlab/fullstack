13b94deb4fb169518eb0cb2cab1ecb72fefda0fb
Nagabhushanam <nagabhushanamn@gmail.com>
2020-12-22 02:21:53 +0530

    AppButton

--- week-4/app1/app/WelcomeScreen.js
+++ week-4/app1/app/WelcomeScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, Text, Image, ImageBackground, StyleSheet } from 'react-native'
 
+import AppButton from './components/AppButton'
+
 function WelcomeScreen(props) {
     return (
         <ImageBackground
@@ -9,11 +11,13 @@ function WelcomeScreen(props) {
         >
             <View style={styles.logoContainer}>
                 <Image style={styles.logo} source={require("./assets/logo-red.png")} />
-                <Text>Sell What You Don't Need</Text>
+                <Text style={styles.tagline}>Sell What You Don't Need</Text>
             </View>
 
-            <View style={styles.loginButton}></View>
-            <View style={styles.registerButton}></View>
+            <View style={styles.buttonsContainer}>
+                <AppButton title="Login" onPress={() => { }} />
+                <AppButton title="Register" onPress={() => { }} color="secondary" />
+            </View>
         </ImageBackground>
     );
 }
@@ -24,10 +28,9 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-end',
         alignItems: 'center'
     },
-    loginButton: {
-        width: '100%',
-        height: 70,
-        backgroundColor: '#fc5c65'
+    buttonsContainer: {
+        padding: 20,
+        width: "100%",
     },
     logo: {
         width: 100,
@@ -39,10 +42,10 @@ const styles = StyleSheet.create({
         top: 70,
         alignItems: 'center'
     },
-    registerButton: {
-        width: "100%",
-        height: 70,
-        backgroundColor: "#4ecdc4",
+    tagline: {
+        fontSize: 25,
+        fontWeight: "600",
+        paddingVertical: 20,
     },
 });
 
