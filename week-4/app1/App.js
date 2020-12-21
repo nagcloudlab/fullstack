f83062035b1e3c2766bf98b3ed84e533bc6a8424
Nagabhushanam <nagabhushanamn@gmail.com>
2020-12-22 00:49:27 +0530

    base app

--- week-4/app1/App.js
+++ week-4/app1/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { View } from 'react-native';
 import WelcomeScreen from './app/WelcomeScreen';
 
 export default function App() {
   return (
-    <View style={{}}>
-      <WelcomeScreen />
-    </View>
+    <WelcomeScreen />
   );
 }
 
--- week-4/app1/app/WelcomeScreen.js
+++ week-4/app1/app/WelcomeScreen.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import { View, Text } from 'react-native'
+import { View, Text, ImageBackground, StyleSheet } from 'react-native'
 
 function WelcomeScreen(props) {
     return (
-        <View>
+        <ImageBackground
+            style={styles.background}
+            source={require("./assets/background.jpg")}
+        >
             <Text>Welcome screen</Text>
-        </View>
+        </ImageBackground>
     );
 }
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
 
 export default WelcomeScreen;
