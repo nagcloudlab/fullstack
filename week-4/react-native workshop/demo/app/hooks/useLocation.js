1279af0fbd74b3e82eee2d9937acc302ee4215c2
Nagabhushanam <nagabhushanamn@gmail.com>
2020-12-23 21:10:29 +0530

    fix

--- week-4/react-native workshop/demo/app/hooks/useLocation.js
+++ week-4/react-native workshop/demo/app/hooks/useLocation.js
@@ -1,30 +1,25 @@
 import { useEffect, useState } from "react";
 import * as Location from "expo-location";
 
-
 export default useLocation = () => {
-
-    const [location, setLocation] = useState(null);
+    const [location, setLocation] = useState();
 
     const getLocation = async () => {
         try {
             const { granted } = await Location.requestPermissionsAsync();
             if (!granted) return;
-            const result = await Location.getLastKnownPositionAsync()
             const {
                 coords: { latitude, longitude },
-            } = await Location.getLastKnownPositionAsync();
+            } = await Location.getCurrentPositionAsync();
             setLocation({ latitude, longitude });
         } catch (error) {
             console.log(error);
         }
     };
 
-
     useEffect(() => {
         getLocation();
     }, []);
 
     return location;
-
-}
+};
