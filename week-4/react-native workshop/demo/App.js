0a780774292cb079f9cba0667806679e26d9cf97
Nagabhushanam <nagabhushanamn@gmail.com>
2020-12-23 23:17:02 +0530

    react native app

--- week-4/react-native workshop/demo/App.js
+++ week-4/react-native workshop/demo/App.js
@@ -2,23 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, Button, Image } from 'react-native';
 
-import ListingEditScreen from "./app/screens/ListingEditScreen";
-import AccountScreen from "./app/screens/AccountScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ListingsScreen from "./app/screens/ListingsScreen";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import LoginScreen from "./app/screens/LoginScreen";
-import RegisterScreen from "./app/screens/RegisterScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import navigationTheme from "./app/navigation/navigationTheme";
 
-import Screen from './app/components/Screen'
+
+
+import AuthNavigator from './app/navigation/AuthNavigator'
+import AppNavigator from './app/navigation/AppNavigator'
+
+
+
 
 export default function App() {
-
-
   return (
-    <ListingEditScreen />
+    <NavigationContainer theme={navigationTheme}>
+      <AppNavigator />
+    </NavigationContainer>
   )
-
 }
