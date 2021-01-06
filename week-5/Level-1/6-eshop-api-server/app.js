c0976707c4e5d2d0b22c5dd085c455b8f6f83039
Nagabhushanam <nagabhushanamn@gmail.com>
2021-01-07 02:28:06 +0530

    swagger, still pending

--- week-5/Level-1/6-eshop-api-server/app.js
+++ week-5/Level-1/6-eshop-api-server/app.js
@@ -6,8 +6,47 @@ const morgan = require('morgan')
 const itemsRouter = require('./routes/items')
 const cartRouter = require('./routes/cart')
 
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
 
 const app = express();
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "eshop api server",
+            version: "0.1.0",
+            description:
+                "This is a simple CRUD API application made with Express and documented with Swagger",
+            license: {
+                name: "MIT",
+                url: "https://spdx.org/licenses/MIT.html",
+            },
+            contact: {
+                name: "Nag",
+                email: "info@email.com",
+            },
+        },
+        servers: [
+            {
+                url: "http://localhost:3000/",
+            },
+        ],
+    },
+    apis: ["./routes/items.js", "./routes/cart.js"],
+};
+
+const specs = swaggerJsdoc(options);
+
+
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs, { explorer: true })
+);
+
 
 // HTTP request looging lib
 app.use(morgan('dev'))
--- week-5/design-patterns/example/index.js
+++ week-5/design-patterns/example/index.js
@@ -1,72 +1,24 @@
 
 
-class StackCalculator {
-    constructor() {
-        this.stack = []
-    }
-    putValue(value) {
-        this.stack.push(value)
-    }
-    getValue() {
-        return this.stack.pop()
-    }
-    peekValue() {
-        return this.stack[this.stack.length - 1]
-    }
-    clear() {
-        this.stack = []
-    }
-    divide() {
-        const divisor = this.getValue()
-        const dividend = this.getValue()
-        const result = dividend / divisor
-        this.putValue(result)
-        return result
-    }
-    multiply() {
-        const multiplicand = this.getValue()
-        const multiplier = this.getValue()
-        const result = multiplier * multiplicand
-        this.putValue(result)
-        return result
-    }
-}
 
-const safeCalculatorHandler = {
-    get: (target, property) => {
-        if (property === 'divide') {
-            return function () {
-                // additional validation logic
-                const divisor = target.peekValue()
-                if (divisor === 0) {
-                    throw Error('Division by 0')
-                }
-                return target.divide()
+
+
+let container = {
+    [Symbol.iterator]: function () {
+        let i = 0;
+        return {
+            next: function () {
+                ++i;
+                let value = i <= 10 ? i : undefined;
+                let done = i <= 10 ? false : true;
+                return { value, done }
             }
         }
-        return target[property]
     }
 }
 
 
 
-const calculator = new StackCalculator()
-const safeCalculator = new Proxy(calculator, safeCalculatorHandler) // 
-
-
-calculator.putValue(3)
-calculator.putValue(2)
-console.log(calculator.multiply()) // 3*2 = 6
-
-safeCalculator.putValue(2)
-console.log(safeCalculator.multiply()) // 6*2 = 12
-
-
-calculator.putValue(0)
-console.log(calculator.divide()) // 12/0 = Infinity
-
-
-safeCalculator.clear()
-safeCalculator.putValue(4)
-safeCalculator.putValue(0)
-console.log(safeCalculator.divide()) // 4/0 -> Error('Division by 0')
+for (let ele of container) {
+    console.log(ele)
+}
