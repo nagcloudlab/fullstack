81d5fd7afef9125bd37b190c11fa2f3c7e200875
Nagabhushanam <nagabhushanamn@gmail.com>
2021-01-08 01:09:51 +0530

    gud-sho app

---
+++ week-6/1-gud-sho/db-init/initialize-database.sql
@@ -0,0 +1,32 @@
+CREATE ROLE super WITH LOGIN PASSWORD 'super';
+
+CREATE ROLE superman WITH LOGIN PASSWORD 'superman' NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
+create database gud_sho_db;
+GRANT ALL PRIVILEGES ON DATABASE gud_sho_db TO superman ;
+GRANT ALL PRIVILEGES ON DATABASE gud_sho_db TO super;
+
+
+
+use gud_sho_db;
+
+create table movies(
+    id int primary key auto_increment,
+    title varchar(50) NOT NULL,
+    duration int
+);
+create table genres(
+    id int primary key auto_increment,
+    genre varchar(50) NOT NULL
+);
+create table movies_genres(
+    movie_id int,
+    genre_id int,
+    FOREIGN KEY (movie_id) references movies,
+    FOREIGN KEY (genre_id) references genres
+);
+insert into genres (genre) values('romance');
+insert into genres (genre) values('adventure');
+insert into genres (genre) values('fantasy');
+
+
+
